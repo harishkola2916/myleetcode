11bf71535f78c88d2ef24f7b87fc1316916a74f1
harish <harishkola2916@gmail.com>
2022-02-02 21:17:25 -0600

    Time: 8 ms (67.90%), Space: 70.6 MB (34.25%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -3,10 +3,10 @@ class Solution {
         HashSet<Integer> dup = new HashSet<>();
         for(int i=0;i<nums.length;i++)
         {
-           if(dup.contains(nums[i]))
+            if(dup.contains(nums[i]))
                return true;
             
-            dup.add(nums[i]);
+           dup.add(nums[i]);
         }
         return false;
     }
