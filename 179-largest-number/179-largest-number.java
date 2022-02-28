36f3c08aae0043da12c881a8481fd6144abf2fcd
harish <harishkola2916@gmail.com>
2022-02-27 19:35:37 -0600

    Time: 4 ms (99.27%), Space: 42 MB (53.46%) - LeetHub

---
+++ 179-largest-number/179-largest-number.java
@@ -0,0 +1,33 @@
+import java.util.Comparator;
+class Solution {
+    public String largestNumber(int[] nums) {
+        
+        String[] arrayOfStr = new String[nums.length];
+        
+        for(int i=0;i<nums.length;i++){
+            arrayOfStr[i] = String.valueOf(nums[i]);
+        }
+        
+        Arrays.sort(arrayOfStr, new Comparator<String>(){ 
+            
+            @Override
+            public int compare(String a, String b){
+                String s1= a+b;
+                String s2 = b+a;
+                return s2.compareTo(s1);
+            }
+            
+        });
+        
+        if(arrayOfStr[0].equals("0"))
+            return "0";
+        
+        String numberStr = new String();
+        for(String num: arrayOfStr){
+            numberStr+=num;
+        }
+        
+        return numberStr;
+    }
+                
+}
