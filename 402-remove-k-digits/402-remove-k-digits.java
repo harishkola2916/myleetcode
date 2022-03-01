8479017d1676fe7ab0d2b101b76a7bedc89e8b41
harish <harishkola2916@gmail.com>
2022-02-28 19:15:44 -0600

    Time: 50 ms (15.61%), Space: 46.6 MB (19.58%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.java
+++ 402-remove-k-digits/402-remove-k-digits.java
@@ -1,38 +1,28 @@
 class Solution {
     public String removeKdigits(String num, int k) {
         
+        StringBuilder sb =  new StringBuilder(num);
+        
         if(k == 0)
             return num;
         
         if(k == num.length())
             return "0";
         
-        Stack<Character> st = new Stack<>();
-        
-        for(int i=0;i<num.length();i++){
-            while(k>0 && !st.isEmpty() && (st.peek()-'a') > (num.charAt(i)-'a')){
-                k--;
-                st.pop();
+        while(k>0){
+            int i=0; // everytime we compare 1st element with the second element and delete the second if it       //is greater than 1st
+            while(i<sb.length()-1 && sb.charAt(i+1)>=sb.charAt(i)){
+                i++;
             }
-            st.push(num.charAt(i));            
-        }
-        while(k > 0 && !st.isEmpty()){
-            st.pop();
+            sb.deleteCharAt(i);
             k--;
         }
         
-        StringBuilder s = new StringBuilder();
-        while(!st.isEmpty()){
-            s.append(st.pop());
-        }
-        
-        s.reverse();
-        
-        while(s.toString().startsWith("0"))
+        while(sb.toString().startsWith("0"))
         {
-           s.deleteCharAt(0); 
+           sb.deleteCharAt(0); 
         }
             
-    return s.length() == 0 ? "0" : s.toString();
+    return sb.length() == 0 ? "0" : sb.toString();
     }
 }
